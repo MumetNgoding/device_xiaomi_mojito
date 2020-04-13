79a99b38d7015e88f55a56e7d3234e63e7187114
Sonal Singh <sonal.singh.19993@gmail.com>
2020-04-13 13:06:48 +0000

    mojito: prebuilts: Add GoogleCameraGo
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I190fa16736e28be15e4100290c15a3004cf7cf94

--- device.mk
+++ device.mk
@@ -71,6 +71,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Gcam
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Force triple frame buffers
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
