468442b55fed44d3002d47c74acf8fb29e64b235
chandu078 <chandudyavanapelli03@gmail.com>
2021-04-19 12:09:59 +0000

    mojito: remove_packages: Drop Some Prebuilt Apps
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Chrome Photos SafetyHubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := arcore CarrierSetup ConnMO DCMO Drive FM2 FilesPrebuilt GoogleTTS libqcomfm_jni Maps MyVerizonServices MaestroPrebuilt NfcNci NgaResources obdm_stub OBDM_Permissions PrebuiltGmail qcom.fmradio RecorderPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt SafetyHubPrebuilt TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib WallpapersBReel2020 YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
