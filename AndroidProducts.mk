2b47fa795e9c7ed9f21680ccd6efa91e564ce2d0
MumetNgoding <bryanhafidz@hack.id>
2021-04-30 00:09:44 +0000

    mojito: BringUp to evox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mojito.mk
+    $(LOCAL_DIR)/evolution_mojito.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_mojito-user \
+    evolution_mojito-userdebug \
+    evolution_mojito-eng
